7158fa652ce4b3b2de138433deeeacd2fc6675aa
Nicolas Siver <nicolas@siver.im>
2018-03-08 00:10:31 -0500

    change logger to use format

--- src/util/logger.js
+++ src/util/logger.js
@@ -13,25 +13,22 @@ const colors = {
     [loggerLevels.SILLY]  : chalk.gray
 };
 
-module.exports = (category, level = loggerLevels.SILLY) => {
-    return new winston.Logger({
-        transports: [
-            new winston.transports.Console({
-                level    : level,
-                timestamp: () => {
-                    let date    = new Date(),
-                        hours   = date.getHours(),
-                        minutes = date.getMinutes(),
-                        seconds = date.getSeconds(),
-                        ms      = date.getMilliseconds();
+const timestamp = () => {
+    let date    = new Date(),
+        hours   = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds(),
+        ms      = date.getMilliseconds();
 
-                    return [pad(2, hours, '0'), pad(2, minutes, '0'), pad(2, seconds, '0'), pad(3, ms, '0')].join(':');
-                },
-                formatter: options => {
-                    return '[' + options.timestamp() + '][' + category + '] ' + (options.message ? colors[options.level](options.message) : '') +
-                        (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
-                }
-            })
-        ]
+    return [pad(2, hours, '0'), pad(2, minutes, '0'), pad(2, seconds, '0'), pad(3, ms, '0')].join(':');
+};
+
+module.exports = (category, level = loggerLevels.SILLY) => {
+    return winston.createLogger({
+        level     : level,
+        format    : winston.format.printf(options => {
+            return `[${timestamp()}][${category}] ${colors[options.level](options.message)}`;
+        }),
+        transports: [new winston.transports.Console()]
     });
 };
